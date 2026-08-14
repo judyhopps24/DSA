08eeb1d61525cd7d4c882a88b847b756b0771f45
judyhopps24 <60727900+judyhopps24@users.noreply.github.com>
2026-08-14 23:43:27 +0530

    Add: merge-k-sorted-linked-lists - submission-3

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-3.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+
+
+public:
+
+
+// iteratively get the smallest among the lists and add to new list. like a pointer based solution
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode* res = new ListNode();
+        ListNode* cur = res;
+
+        auto comp = [](ListNode* a, ListNode* b){
+            return a->val> b->val;
+        };
+
+        priority_queue<ListNode*, vector<ListNode*>, decltype(comp)> pq(comp);
+        for(int i =0 ; i<lists.size() ;i++){
+            if(lists[i] == NULL) continue;
+            pq.emplace(lists[i]);
+        }
+
+        while(!pq.empty()){
+            ListNode* p = pq.top();
+            pq.pop();
+
+            cur->next = p;
+            p = p->next;
+            if(p!=NULL)
+                pq.emplace(p);
+            cur=cur->next;
+        }
+        return res->next;
+    }
+};
