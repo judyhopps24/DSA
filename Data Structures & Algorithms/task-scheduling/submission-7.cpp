3f10c4f64bb90451a0fcc22604968d4f2b75f421
judyhopps24 <60727900+judyhopps24@users.noreply.github.com>
2026-08-14 11:21:11 +0530

    Add: task-scheduling - submission-7

---
+++ Data Structures & Algorithms/task-scheduling/submission-7.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char, int>freq;
+        for(int i=0;i<tasks.size();i++){
+            freq[tasks[i]]++;
+        }
+        int globalTime = 1;
+        priority_queue<pair<int, char>> pq;
+        for(auto x: freq){
+            if(x.second != 0){
+                pq.push({x.second, x.first});
+            }
+        }
+        queue<pair<char, int>> waitTimeQueue;
+        while(!waitTimeQueue.empty() || !pq.empty()){
+            if(!waitTimeQueue.empty()){
+                // cout<<" wait list is not epmty \n";
+                pair<char, int> waitQTop = waitTimeQueue.front();
+                //  cout<<"waitQTop : "<< waitQTop.first<<" "<<waitQTop.second<<"\n";
+                if(waitQTop.second <= globalTime){
+                    // cout<<"waitQTop is ready: \n";
+                    waitTimeQueue.pop();
+                    // cout<<" puhsing to pq\n";
+                    pq.push({freq[waitQTop.first], waitQTop.first});
+                } 
+            }
+
+            if(!pq.empty()){
+                
+                // cout<<"pq is not empty\n";
+                pair<int, char> top = pq.top();
+                // cout<<"pq top :"<<top.second<<" : fre: "<<top.first<<"\n";
+                pq.pop();
+
+                freq[top.second]--;
+                // cout<<" reducing freq \n";
+                if(freq[top.second]!=0){
+                    // cout<<"freq is not 0 adding to waitList \n";
+                    waitTimeQueue.emplace(top.second, globalTime+n+1);
+                }  
+            }
+             globalTime++;
+                
+        }
+
+        return globalTime-1;
+
+    }
+};
