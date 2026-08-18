295928ae1b643a0a7988a52a690ceb07723c9ce1
judyhopps24 <60727900+judyhopps24@users.noreply.github.com>
2026-08-18 20:53:08 +0530

    Add: trapping-rain-water - submission-4

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> prefix(n, 0);
+        vector<int> suffix(n,0);
+        int sum = 0;
+
+        prefix[0]= height[0];
+        suffix[n-1] = height[n-1];
+        for(int i=1;i<n;i++)
+        {
+            prefix[i] = max(prefix[i-1], height[i]);
+            suffix[n-i-1] = max(suffix[n-i], height[n-i-1]);
+        }
+
+        // for(auto x: prefix) cout<<x<< " ";
+        // cout<<"\n";
+
+
+        // for(int i=n-2;i>=0;i--)
+        // {
+        //     suffix[i] = max(suffix[i+1], height[i]);
+        // }
+        // for(auto x: suffix) cout<<x<< " ";
+        // cout<<"\n";
+
+        for(int i=1;i<n-1;i++){
+            // cout<<"for index: "<<i<<" adding : "<<min(prefix[i-1], suffix[i+1]) - height[i]<<"\n";
+            int waterLevel = min(prefix[i-1], suffix[i+1]) - height[i];
+            if(waterLevel > 0)
+                sum += waterLevel;
+        }
+        return sum;
+    }
+};
