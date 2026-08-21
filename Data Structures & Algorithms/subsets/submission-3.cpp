30b5292d8a2c9bd76978c977daad0b0736cda7a4
judyhopps24 <60727900+judyhopps24@users.noreply.github.com>
2026-08-21 21:15:08 +0530

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        vector<int> subset;
+        recurse(nums, 0, n, ans, subset);
+        return ans;
+    }
+    void recurse(vector<int>& nums, int i,  int n, vector<vector<int>>& ans,  vector<int> subset){
+        if(i>=n)
+        {
+            ans.push_back(subset);
+            return;
+        }
+
+        recurse(nums, i+1, n, ans, subset);
+        
+
+        subset.push_back(nums[i]);
+        recurse(nums, i+1,n,ans, subset);
+    }
+};
+
+
+
+
